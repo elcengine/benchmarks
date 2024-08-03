12fd117d5937fb3a04bd787fe828a32e3acf178e
Akalanka Perera <akalankaperera128@gmail.com>
2024-08-03 14:14:16 +0530

    Feat: added benchmark script

---
+++ benchmark.sh
@@ -0,0 +1,23 @@
+copy_files() {
+    local src_dir="$1"
+    local dest_dir="$2"
+
+    for item in "$src_dir"/*; do
+        base_name=$(basename "$item")
+        new_name="${base_name/_test/}"
+        if [ -d "$item" ]; then
+            mkdir -p "$dest_dir/$new_name"
+            copy_files "$item" "$dest_dir/$new_name"
+        else
+            cp "$item" "$dest_dir/$new_name"
+        fi
+    done
+}
+
+destination="./.elemental"
+
+rm -rf $destination && mkdir -p "$destination"
+
+copy_files "./tests" "$destination"
+
+go test -v ./...
